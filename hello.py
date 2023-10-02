cf756d6685c11f1aa9ff044f6f02b3cf1357fd4d
Jin Hong YONG <34515006+jjinhongg@users.noreply.github.com>
2023-10-02 12:40:09 +0000

    add: .gitignore, new functions and test cases

--- hello.py
+++ hello.py
@@ -1,2 +1,16 @@
 def more_hello():
     return "hi"
+
+def more_goodbye():
+    return "bye"
+
+
+if __name__ == "__main__":
+    more_hello()
+
+# This is nonsense code that generates a warning
+# var = 1
+# var = var
+
+# Bad syntax
+# foo =
